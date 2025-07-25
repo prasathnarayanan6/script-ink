7dbfd279fd801a3086c46dfd2772a67fc42f3047
Prasath Narayanan <prasathnarayanan6@gmail.com>
2025-07-26 00:08:33 +0530

    changes

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,13 +1,17 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
+import hero from '../assets/hero.jpeg'
 
 function Login(){
     return (
         <>
-           <div className="bg-pink-200 h-screen">
+           <div className="bg-black h-screen">
                         <div className="fixed top-0 left-0 w-full z-50 bg-transparent">
                                 <NavBar />
                         </div>
+                        <section className="relative overflow-hidden text-gray-800 min-h-[calc(100vh-64px)] flex items-center justify-center">
+                            
+                        </section>
            </div>     
         </>
     )
